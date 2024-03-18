ca2e66a7cfde57bb2da86b07ec17f6a79785feda
sanchesmp <55552815+sanchesmp@users.noreply.github.com>
2024-03-18 18:25:02 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors'; // Importe o pacote cors
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Configure o CORS para permitir solicitações de todas as origens
+
+  // Configuração do CORS para permitir solicitações de todas as origens
   app.use(cors({
-    origin: '*', // Permite solicitações de todas as origens
+    origin: true, // Permitir solicitações de todas as origens
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
   }));
+
   await app.listen(3000);
 }
 bootstrap();
