eb2fbf9a00b41dd0ea599042b4d16083fb7e28a3
sanchesmp <55552815+sanchesmp@users.noreply.github.com>
2024-03-18 17:38:07 -0300

    enable cors

--- src/veiculo/veiculo.module.ts
+++ src/veiculo/veiculo.module.ts
@@ -3,6 +3,7 @@ import { VeiculoService } from './veiculo.service';
 import { VeiculoController } from './veiculo.controller';
 import { PrismaClient } from '@prisma/client';
 
+
 @Module({
   controllers: [VeiculoController],
   providers: [VeiculoService, PrismaClient],
